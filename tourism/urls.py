03550d29e94e78783d0e43a64c11b481311e7b03
Maun <julien.nonin@sfr.fr>
2020-08-07 12:21:22 +0200

    (Feat) lazy load of markers

--- tourism/urls.py
+++ tourism/urls.py
@@ -6,4 +6,5 @@ app_name = 'tourism'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('get/ajax/poi', views.detail, name='poi_detail'),
+    path('get/ajax/lazy', views.visible_poi, name='poi_load')
 ]
--- tourism/views.py
+++ tourism/views.py
@@ -2,7 +2,11 @@ from django.core import serializers
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+
+from django.contrib.gis.geos import Polygon
+
 from .models import PointOfInterest
+import json
 
 class IndexView(ListView):
     template_name = 'tourism/index.html'
@@ -28,5 +32,16 @@ def detail(request):
         poi_id = request.GET.get("poi_id", None)
         poi = get_object_or_404(PointOfInterest, pk=poi_id)
         return render(request, 'tourism/index/_detail.html', {'variable': poi})
+    else:
+        return HttpResponseNotAllowed(('GET',))
+
+def visible_poi(request):
+    if request.is_ajax and request.method == "GET":
+        bounds = json.loads(request.GET.get("bounds", None))
+        sw, ne = bounds["_southWest"], bounds["_northEast"]
+        bbox = (sw["lng"], sw["lat"], ne["lng"], ne["lat"])
+        geom = Polygon.from_bbox(bbox)
+        poi_list = PointOfInterest.objects.filter(location__contained=geom)
+        return render(request, 'tourism/index/_poi_loader.html', {'poi_list': poi_list})
     else:
         return HttpResponseNotAllowed(('GET',))
